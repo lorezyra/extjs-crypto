c44929a4c1e159ae158cf10f6c9685aa51117428
LoreZyra <Rich@RichieBartlett.com>
2014-07-11 12:04:05 +0900

    Completely refactored to match RFC guidelines
    - Added core functions and properties to the Crypto core.
    - Added alternate (URL safe) encoding table to Base64
    
    //TODO: add SHA1, SHA2, and HMAC_SHA, and MD5

--- Crypto.js
+++ Crypto.js
@@ -35,7 +35,65 @@ Ext.define('Ext.Crypto', {
 		return this[type](value);
 	},
 */
+
+	charSets: {
+     	/* CharEncodingISO Latin Alphabet No. 1, a.k.a. ISO-LATIN-1. */
+		ISO_8859_1 : "ISO-8859-1",
+
+     	/* Seven-bit ASCII, also known as ISO646-US, also known as the Basic Latin block of the Unicode character set. */
+		US_ASCII : "US-ASCII",
+
+     	/*      * Sixteen-bit Unicode Transformation Format, The byte order specified by a mandatory initial byte-order mark
+     			* (either order accepted on input, big-endian used on output) */
+		UTF_16 : "UTF-16",
+
+     	/* Sixteen-bit Unicode Transformation Format, big-endian byte order. */
+		UTF_16BE : "UTF-16BE",
+
+     	/* Sixteen-bit Unicode Transformation Format, little-endian byte order. */
+		UTF_16LE : "UTF-16LE",
+
+     	/* Eight-bit Unicode Transformation Format. */
+		UTF_8 : "UTF-8"
+	},
 	
+    /**
+     *  MIME chunk size per RFC 2045 section 6.8.
+     */
+	MIME_CHUNK_SIZE: 76,
+
+	/**
+     * PEM chunk size per RFC 1421 section 4.3.2.4.
+     */
+	PEM_CHUNK_SIZE: 64,
+
+    /**
+     * Defines the default buffer size - currently {@value}
+     * - must be large enough for at least one encoded block+separator
+     */
+	DEFAULT_BUFFER_SIZE: 8192,
+
+	
+    /**
+     * Checks if a byte value is whitespace or not.
+     * Whitespace is taken to mean: space, tab, CR, LF
+     * @param byteToCheck
+     *            the byte to check
+     * @return true if byte is whitespace, otherwise false
+     */
+	isWhiteSpace: function (byteToCheck) {
+        switch (byteToCheck) {
+            case ' ' :
+            case '\n' :
+            case '\r' :
+            case '\t' :
+                return true;
+            default :
+                return false;
+        }
+    },
+
+   //NOTE: only supports US_ASCII; not UTF_8 safe
 	reverse: function (/*String*/ strg){//v2.0
 		// reverses the order of the letters in passed string strg
 		// Example: "hello" becomes "olleh"...
@@ -47,7 +105,6 @@ Ext.define('Ext.Crypto', {
 	   return(temp);
 	   */
 	   //convert string to array and return
-	   //NOTE: only supports ASCII; not UTF8 safe
 	   return strg.split('').reverse().join('');
 	}//end function reverseString
 
--- Crypto/Base64.js
+++ Crypto/Base64.js
@@ -4,7 +4,9 @@
  * @license   IP of Richie Bartlett, Jr. (Rich@RichieBartlett.com) [All Rights Reserved.]
  
  Inspired by: https://github.com/brainfucker/node-base64/blob/master/js_base64_for_comparsion.js
-  Technical white paper on base64 implementation: http://www.ietf.org/rfc/rfc3548.txt
+    		  http://svn.apache.org/repos/asf/webservices/commons/trunk/modules/util/
+			  https://commons.apache.org/proper/commons-codec/apidocs/src-html/org/apache/commons/codec/binary/BaseNCodec.html
+  Technical white paper on base64 implementation: http://www.ietf.org/rfc/rfc3548.txt & http://www.ietf.org/rfc/rfc2045.txt
  */
 
 
@@ -13,10 +15,114 @@ Ext.define('Ext.Crypto.Base64', {
 	alternateClassName: ['Ext.Crypto.base64'],
 	requires: 'Ext.Crypto',
 	singleton : true,
-	
-	keyMap: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
 
-	encode: function(value) {
+	
+	/* End Of File code*/
+	EOF: -1,
+
+    /**
+     * BASE32 characters are 6 bits in length.
+     * They are formed by taking a block of 3 octets to form a 24-bit string,
+     * which is converted into 4 BASE64 characters.
+     */
+    BITS_PER_ENCODED_BYTE: 6,
+    BYTES_PER_UNENCODED_BLOCK: 3,
+    BYTES_PER_ENCODED_BLOCK: 4,
+
+    /* Chunk separator per RFC 2045 section 2.1. */
+	CHUNK_SEPARATOR: ['\r', '\n'],
+	
+    /* Byte used to pad encoded output. */
+	PAD_DEFAULT: '=',
+	PAD: PAD_DEFAULT, // instance variable just in case it needs to vary later
+	
+	/*  */
+	STANDARD_ENCODE_TABLE: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
+
+    /**
+     * This is a copy of the STANDARD_ENCODE_TABLE above, but with + and /
+     * changed to - and _ to make the encoded Base64 results more URL-SAFE.
+     * This table is only used when the Base64's mode is set to URL-SAFE.
+     */
+	URL_SAFE_ENCODE_TABLE: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_",
+
+    /**
+     * This array is a lookup table that translates Unicode characters drawn from the "Base64 Alphabet" (as specified
+     * in Table 1 of RFC 2045) into their 6-bit positive integer equivalents. Characters that are not in the Base64
+     * alphabet but fall within the bounds of the array are translated to -1.
+     *
+     * Note: '+' and '-' both decode to 62. '/' and '_' both decode to 63. This means decoder seamlessly handles both
+     * URL_SAFE and STANDARD base64. (The encoder, on the other hand, needs to know ahead of time what to emit).
+     *
+     * Thanks to "commons" project in ws.apache.org for this code.
+     * http://svn.apache.org/repos/asf/webservices/commons/trunk/modules/util/
+     */
+    DECODE_TABLE: [
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+            -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, 62, -1, 63, 52, 53, 54,
+            55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4,
+            5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+            24, 25, -1, -1, -1, -1, 63, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34,
+            35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51
+    ],
+	
+    /** Mask used to extract 6 bits; used when encoding */
+	MASK_6BITS: 0x3f,
+
+    /* Convenience variable to help us determine when our buffer is going to run out of room and needs resizing. */
+	decodeSize: 0,
+
+    /* Convenience variable to help us determine when our buffer is going to run out of room and needs resizing. */
+	encodeSize: 0,
+	
+    /**
+     * Chunksize for encoding. Not used when decoding.
+     * A value of zero or less implies no chunking of the encoded data.
+     * Rounded down to nearest multiple of encodedBlockSize.
+     */
+	lineLength: 0,
+
+    /**
+     * Size of chunk separator. Not used unless {@link #lineLength} &gt; 0.
+     */
+    chunkSeparatorLength: 1,
+	
+	/* default the table to use for codec */
+	encodeTable: STANDARD_ENCODE_TABLE,
+	
+	/* table to use for codec with default pad code*/
+	keyMap: encodeTable + PAD_DEFAULT,
+
+
+
+
+	/**
+	 * setup the base64 en/decoder with user passed configs (if any)
+	 */
+	constructor: function (config) {
+        config = config || {};
+        Ext.apply(this, config);
+		
+		setKeyMap();
+	},
+	
+
+	/**
+	 * Function resets the keyMap to the encodeTable config and adds the Pad code
+	 */
+	setKeyMap: function () {
+        this.keyMap = this.encodeTable + this.PAD;
+    },
+
+	/**
+	 * Function returns the boolean value if url-safe table is used for encoding
+	 */
+	isUrlSafe: function () {
+        return this.encodeTable == this.URL_SAFE_ENCODE_TABLE;
+    },
+
+	encode: function (value) {
 		input = escape(value);
 		var chr1,
 			chr2,
@@ -34,17 +140,17 @@ Ext.define('Ext.Crypto.Base64', {
 			chr3 = input.charCodeAt(i++);
 			
 			enc1 = chr1 >> 2;
-			enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-			enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-			enc4 = chr3 & 63;
+			enc2 = ((chr1 & BYTES_PER_UNENCODED_BLOCK) << BYTES_PER_ENCODED_BLOCK) | (chr2 >> BYTES_PER_ENCODED_BLOCK);
+			enc3 = ((chr2 & 15) << 2) | (chr3 >> BITS_PER_ENCODED_BYTE);
+			enc4 = chr3 & MASK_6BITS;
 			
 			if (isNaN(chr2)) {
-				enc3 = enc4 = 64;
+				enc3 = enc4 = MASK_6BITS + 1;
 			} else if (isNaN(chr3)) {
-				enc4 = 64;
+				enc4 = MASK_6BITS + 1;
 			}
 			
-			output += this.keyMap.charAt(enc1) +
+			output += this.encodeTable.charAt(enc1) +
 				this.keyMap.charAt(enc2) +
 				this.keyMap.charAt(enc3) +
 				this.keyMap.charAt(enc4);
@@ -55,8 +161,9 @@ Ext.define('Ext.Crypto.Base64', {
 		return output;
 	},
 	
-	decode: function(value) {
-		var chr1,
+	decode: function (value) {
+		var base64test,
+			chr1,
 			chr2,
 			chr3 = "",
 			enc1,
@@ -66,16 +173,36 @@ Ext.define('Ext.Crypto.Base64', {
 			output = "",
 			i = 0;
 		
-		var base64test = /[^A-Za-z0-9\+\/\=]/g;
-		if ( base64test.exec(value) ) {
-			//<debug>
-			Ext.Error.raise("There were invalid base64 characters in the input text.\n" +
-			"Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
-			"Expect errors in decoding.");
-			//</debug>
+		if ( isURLsafe() ) {
+
+			base64test = /[^A-Za-z0-9\-\_\=]/g;
+
+			if ( base64test.exec(value) ) {
+				//<debug>
+				Ext.Error.raise("There were invalid base64 characters in the input text.\n" +
+				"Valid base64 (URL safe) characters are A-Z, a-z, 0-9, '-', '_',and '" + this.PAD + "'\n" +
+				"Expect errors in decoding.");
+				//</debug>
+			}
+
+			// remove all characters that are not in the URL safe "alphabet": A-Z, a-z, 0-9, -, _, or =
+			input = value.replace(base64test, "");
+
+		} else {
+
+			base64test = /[^A-Za-z0-9\+\/\=]/g;
+
+			if ( base64test.exec(value) ) {
+				//<debug>
+				Ext.Error.raise("There were invalid base64 characters in the input text.\n" +
+				"Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '" + this.PAD + "'\n" +
+				"Expect errors in decoding.");
+				//</debug>
+			}
+
+			// remove all characters that are not in the Standard "alphabet": A-Z, a-z, 0-9, +, /, or =
+			input = value.replace(base64test, "");
 		}
-		// remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-		input = value.replace(/[^A-Za-z0-9\+\/\=]/g, "");
 		
 		do {
 			enc1 = this.keyMap.indexOf(input.charAt(i++));
@@ -83,16 +210,16 @@ Ext.define('Ext.Crypto.Base64', {
 			enc3 = this.keyMap.indexOf(input.charAt(i++));
 			enc4 = this.keyMap.indexOf(input.charAt(i++));
 			
-			chr1 = (enc1 << 2) | (enc2 >> 4);
-			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-			chr3 = ((enc3 & 3) << 6) | enc4;
+			chr1 = (enc1 << 2) | (enc2 >> BYTES_PER_ENCODED_BLOCK);
+			chr2 = ((enc2 & 15) << BYTES_PER_ENCODED_BLOCK) | (enc3 >> 2);
+			chr3 = ((enc3 & BYTES_PER_UNENCODED_BLOCK) << BITS_PER_ENCODED_BYTE) | enc4;
 			
 			output += String.fromCharCode(chr1);
 			
-			if (enc3 != 64) {
+			if (enc3 != MASK_6BITS + 1) {
 				output += String.fromCharCode(chr2);
 			}
-			if (enc4 != 64) {
+			if (enc4 != MASK_6BITS + 1) {
 				output += String.fromCharCode(chr3);
 			}
 			
@@ -102,6 +229,15 @@ Ext.define('Ext.Crypto.Base64', {
 		} while (i < input.length);
 		
 		return unescape(output);
-	}
+	},
+	
+    /**
+     * Returns whether or not the octet is in the base 64 alphabet.
+     *
+     * @param octet
+	 */
+	isBase64: function (octet) {
+        return octet == PAD_DEFAULT || (octet >= 0 && octet < DECODE_TABLE.length && DECODE_TABLE[octet] != -1);
+    }
 
 });
